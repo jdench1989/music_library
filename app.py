b5d8f1c6a0b7123eaec99750930fcaf710895ab9
Jack Dench <jdench1989@gmail.com>
2025-02-18 09:57:43 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from lib.database_connection import DatabaseConnection
+from lib.album_repository import AlbumRepository
 from lib.artist_repository import ArtistRepository
 
 
@@ -16,3 +17,11 @@ artists = artist_repository.all()
 # List them out
 for artist in artists:
     print(artist)
+
+# Retrieve all albums
+album_repository = AlbumRepository(connection)
+albums = album_repository.all()
+
+# List them out
+for album in albums:
+    print(album)
